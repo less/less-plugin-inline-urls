1856aab25778ce061d88cf0ff949780be16cf7b0
Luke Page <luke.a.page@gmail.com>
2014-09-08 14:48:05 +0100

    Remove auto-prifixer and make package just about the inline images plugin

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-less-plugins.js
-===============
+less-plugin-inline-images
+=========================
+
+Converts url("image.png") to data-uri's automatically, without having to write data-uri("image.png") in your less
--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var lessTest = require("less/test/less-test"),
     lessTester = lessTest(),
-    plugins = require('../lib/index.js'),
+    plugins = require('../lib'),
     stylize = require('less/lib/less/lessc_helper').stylize;
 
 console.log("\n" + stylize("LESS - Plugins", 'underline') + "\n");
@@ -8,6 +8,3 @@ console.log("\n" + stylize("LESS - Plugins", 'underline') + "\n");
 lessTester.runTestSet(
     {strictMath: true, relativeUrls: true, silent: true, plugins: [new plugins.InlineImages()] },
     "inline-images/");
-lessTester.runTestSet(
-    {strictMath: true, relativeUrls: true, silent: true, plugins: [new plugins.AutoPrefix()] },
-    "auto-prefix/");
